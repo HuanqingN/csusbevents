fd92745ec58d408288a2d7f7376d4b28a0d012d0
H.Q. NONG <ncusknonghuanqing@163.com>
2020-02-22 11:05:41 -0800

    Simple login page

---
+++ button.js
@@ -0,0 +1,20 @@
+function on_submit(){
+    var userid, pwd;
+    userid = document.getElementById("userid");
+    pwd = document.getElementById("pwd");
+
+    // var url = "localhost:";
+    // var xhr = new XMLHttpRequest();
+    // xhr.open("GET", url, false);
+    // xhr.onload = // something
+    document.getElementById("your_button's_ID").addEventListener("click",
+    function() {xhr.send(data)},
+    false
+);
+    // document.getElementById("submitted").innerHTML="submitted";
+}
+
+
+// function onload(){
+//     document.getElementById("submit").onclick = onsubmit();
+// }
