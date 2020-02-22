95893a0fac3600d81fb009d901fe57b93157fc8c
H.Q. NONG <ncusknonghuanqing@163.com>
2020-02-22 13:11:25 -0800

    event.html added

--- button.js
+++ button.js
@@ -1,16 +1,19 @@
 function on_submit(){
-    var userid, pwd;
-    userid = document.getElementById("userid");
-    pwd = document.getElementById("pwd");
+    if(document.getElementById("student").checked){
+        location.href = "events.html";
+    } else {
+            var userid, pwd;
+            userid = document.getElementById("userid");
+            pwd = document.getElementById("pwd");
+    }
 
     // var url = "localhost:";
     // var xhr = new XMLHttpRequest();
     // xhr.open("GET", url, false);
     // xhr.onload = // something
-    document.getElementById("your_button's_ID").addEventListener("click",
-    function() {xhr.send(data)},
-    false
-);
+    // document.getElementById("your_button's_ID").addEventListener("click",
+    // function() {xhr.send(data)},
+    // false;
     // document.getElementById("submitted").innerHTML="submitted";
 }
 
